e659253dcc8990626b5da8ca0c765e69810718cc
fartlord21 <103141506+fartlord21@users.noreply.github.com>
2022-04-29 01:13:14 -0400

    added difficulty choices
    couldnt decide/figure out which was better for the page get/post

--- functions.php
+++ functions.php
@@ -1,6 +1,71 @@
 <?php 
 
-function selectDifficulty() {
+function displaysDifficultyChoices() {
+  global $connection;
+
+  $query = "SELECT * FROM difficultyChoices";
+
+  $result = mysqli_query($connection,$query);
+
+  if(!$result) {
+    die ('query was a failure ma boi.');
+  }
+  else {
+    while($difficulty=mysqli_fetch_assoc($result)) {
+      $choices=$difficulty['choices'];
+      echo "<option>$choices</option>";
+    }
+  }
+}
+
+//some combination of these two makes function selectsDifficulty
+function selectsDifficulty() {
+  if(isset($_POST['submit'])) {
+    global $connection;
+
+    $difficulty= $_POST['difficulty'];
+
+    if(!$difficulty) {
+      die ('failure to select difficulty' . mysqli_error($connection));
+    }
+    else {
+      print_r($_GET);
+      echo "<br>let's set a ";
+    }
+  /* function returnsDataForUpdating() {//no condiitinal for running bc we want it to load as the page loads so the user can pick from a list. 
+global $connection;//this can be done instead of $connection=$_POST["$blahBLah"]; you would usually do, this is the way to do it with variables created in other files. these files have to be included though
+
+$query = "SELECT * FROM users";
+
+$result = mysqli_query($connection,$query);
+
+if(!$result) {
+  die ('query was a failure ma boi.');
+  }
+else {
+  while($row=mysqli_fetch_assoc($result)) { //we want to see the row relating to the result
+    $id=$row['id'];//this plus the fact that row is used in the parameter for the while loop, allows us to basically cycle through all the id's i the database we are accessing
   
+    echo "<option value='$id'>$id</option>";
+  }
+ }
+}
+
+function readsData() { 
+global $connection;
+
+$query="SELECT * FROM users";
+
+$result=mysqli_query($connection,$query);
+
+if(!$result) {
+  die ('query was a failure ma boi.');
+  }
+while($row=mysqli_fetch_assoc($result)) {//can use ysqli_fetch_row or mysqli_fetch_assoc whatver fits best  
+  print_r($row);
+  }
+}
+*/
+ }
 }
 ?>
--- startDifficulty.php
+++ startDifficulty.php
@@ -1,9 +1,27 @@
+<!-- $_GET is going to be useful once the diff and username is set, get wil be used to play w the arrays variables for the game -->
 <?php include "db.php"; ?>
 <?php include "functions.php"; ?>
+
 <?php include "includes/headerStartDifficulty.php"; ?>
 
 <div class="container">
-  
-</div>
-
+  <div>
+   <h1>Select Your Difficulty</h1>
+   <br>
+    <form action="startDifficulty.php" method="post">
+      <div class= "form-group">
+        <!--<label for="difficulty">Difficulty</label>-->
+        <select name= "difficulty" id= "">
+          <?php
+          displaysDifficultyChoices();
+          ?>
+        </select>
+      </div>
+    </form>
+      <br>
+      <a href="startDifficulty.php?choices=<?php echo $difficulty; ?>">Select This Difficulty</a>
+     <input type="submit" name="submit" class="btn btn-primary" value="Select This Difficulty">
+   <?php selectsDifficulty(); ?>
+   <a href="startUsername.php">username</a>
+ </div>
 <?php include "includes/footerStart.php"; ?>
